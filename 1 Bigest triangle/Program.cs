bf4c36fa213edafa4f06f8816526f4bef95137b0
Илья Пермиловский <lannyshark@ya.ru>
2021-11-23 00:51:45 +0300

    Добавьте файлы проекта.

---
+++ 1 Bigest triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Bigest_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон первого треугольника");
+            double a1 = Convert.ToInt32(Console.ReadLine());
+            double b1 = Convert.ToInt32(Console.ReadLine());
+            double c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длины сторон второго треугольника");
+            double a2 = Convert.ToInt32(Console.ReadLine());
+            double b2 = Convert.ToInt32(Console.ReadLine());
+            double c2 = Convert.ToInt32(Console.ReadLine());
+            if (CalcSquare(a1, b1, c1) > CalcSquare(a2, b2, c2))
+                Console.WriteLine("Площадь первого треуголника больше и равна {0:F2}", CalcSquare(a1, b1, c1));
+            else if (CalcSquare(a1, b1, c1) < CalcSquare(a2, b2, c2))
+                Console.WriteLine("Площадь второго треуголника больше и равна {0:F2}", CalcSquare(a2, b2, c2));
+            else
+                Console.WriteLine("Площадь треуголников одинаковая и равна {0:F2}", CalcSquare(a1, b1, c1));
+            Console.ReadKey();
+        }
+        static double CalcSquare(double a, double b, double c)
+        {
+            double s = Math.Sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
+            return s;
+        }
+    }
+}
---
+++ 2 Cube/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Cube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int v, s;
+            CalcVolSquare(a, out v, out s);
+            Console.WriteLine("Объем: {0} \nПлощадь: {1}",v,s);
+            Console.ReadKey();
+        }
+        static void CalcVolSquare(int a, out int v, out int s)
+        {
+            v = a * a * a;
+            s = 6 * a * a;
+        }
+    }
+}
